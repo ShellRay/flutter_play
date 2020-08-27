ac50b2174850b8b4568357510b752e17785e50e4
xiewen <xiewen@17guagua.com>
2020-08-27 21:47:04 +0800

    flutter play personalCenterPage update better

--- lib/views/personal_center_page/personal_center_page.dart
+++ lib/views/personal_center_page/personal_center_page.dart
@@ -4,11 +4,20 @@ import 'package:flutter_play/utils/UiUtils.dart';
 
 class PersonalCenterPage extends StatefulWidget {
   PersonalCenterPage();
+
   TapGestureRecognizer tapGestureRecognizer = new TapGestureRecognizer();
+
   _PersonalCenterPage createState() => _PersonalCenterPage();
 }
 
 class _PersonalCenterPage extends State<PersonalCenterPage> {
+   String userAvatar =
+      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598370655632&di=e7c22fa21b30e2f5030216bd36e9d9f2&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201508%2F18%2F20150818213048_vAdhz.jpeg";
+   var userName;
+
+   var items=["红包签到","会员中心","私房红包","U先试用","我的服务","领券中心","天天夺宝","设置中心"];
+   var itemIcons=[Icons.print,Icons.stay_current_portrait,Icons.credit_card,Icons.speaker_notes,Icons.room_service,Icons.golf_course,Icons.attach_money,Icons.settings];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -29,22 +38,57 @@ class _PersonalCenterPage extends State<PersonalCenterPage> {
         new SliverAppBar(
           pinned: true,
           expandedHeight: 200.0,
-          flexibleSpace: const FlexibleSpaceBar(
-            title: const Text('personal'),
+          primary: true,
+          //是否显示在状态栏的下面,false就会占领状态栏的高度 指的的是标题栏部分
+          backgroundColor: Colors.cyanAccent,
+          leading: new IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () {},
           ),
+          actions: <Widget>[
+            new IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                print("添加");
+              },
+            ),
+            new IconButton(
+              icon: Icon(Icons.more_horiz),
+              onPressed: () {
+                print("更多");
+              },
+            ),
+          ],
+//          iconTheme: new IconThemeData(color: Colors.transparent),//标题栏的style 设置透明的话leading actions不会显示
+          flexibleSpace:
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: [Colors.blueAccent, Colors.lightGreenAccent, Colors.yellowAccent],
+              ),
+            ),
+            child:_rengerSliverAppBarColumn(),
+          ),
+          //添加背景图片
+//          new FlexibleSpaceBar(
+//            background: Image.asset("assets/images/p2.png", fit: BoxFit.fill),
+//          ),
+
         ),
 
-        new SliverPadding(padding: EdgeInsets.all(0.01),
-            sliver: SliverToBoxAdapter(
-            child: Container(
+        new SliverPadding(
+          padding: EdgeInsets.all(0.01),
+          sliver: SliverToBoxAdapter(
+              child: Container(
 //            child: new Text("fdkjfk"),
             height: 8,
             width: MediaQuery.of(context).size.width,
             color: Colors.white,
-        )),
+          )),
         ),
 
-        new SliverPadding(padding: EdgeInsets.symmetric(horizontal: 5.0),
+        new SliverPadding(
+          padding: EdgeInsets.symmetric(horizontal: 5.0),
           sliver: new SliverGrid(
             gridDelegate:
 //      new SliverGridDelegateWithMaxCrossAxisExtent(
@@ -54,20 +98,19 @@ class _PersonalCenterPage extends State<PersonalCenterPage> {
 //        childAspectRatio: 4.0,
 //
 //      ),
-            new SliverGridDelegateWithFixedCrossAxisCount(
+                new SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisCount: 4,
-              childAspectRatio: 2,//纵轴缩放比例
-              crossAxisSpacing: 3.0,
+              childAspectRatio: 2, //纵轴缩放比例
+              crossAxisSpacing: 8.0,
               mainAxisSpacing: 10.0,
             ),
             delegate: new SliverChildBuilderDelegate(
-                  (BuildContext context, int index) {
+              (BuildContext context, int index) {
                 return new Container(
                   alignment: Alignment.center,
                   height: 15,
-                  color: Colors.teal[100 * (index % 9)],
+                  color: Colors.lightGreen[100 * (index % 9)],
                   child: _rengerGridView(index),
-
                 );
               },
               childCount: 4,
@@ -75,46 +118,168 @@ class _PersonalCenterPage extends State<PersonalCenterPage> {
           ),
         ),
 
-
-
-        new SliverPadding(padding: EdgeInsets.all(0.1),
+        new SliverPadding(
+          padding: EdgeInsets.all(0.1),
           sliver: SliverToBoxAdapter(
               child: Container(
-                height: 8,
-                width: MediaQuery.of(context).size.width,
-                color: Colors.white,
-              )),
+            height: 8,
+            width: MediaQuery.of(context).size.width,
+            color: Colors.white,
+          )),
         ),
 
         new SliverFixedExtentList(
           itemExtent: 50.0,
           delegate: new SliverChildBuilderDelegate(
             (BuildContext context, int index) {
-              return new Container(
+              return new InkWell(
+                  onTap: (){
+                    _sliverFixedExtentItemClick(index);
+                  },
+                  child:new Container(
                 alignment: Alignment.center,
-                color: Colors.lightBlue[100 * (index % 9)],
-                child: new Text('list item $index'),
-              );
+                height: 50,
+//                color: Colors.white[100 * (index % 9)],
+                color: Colors.white,
+                child: new Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      width: 18,
+                    ),
+
+                    Text.rich(
+                      TextSpan(
+                        children: <InlineSpan>[
+                          WidgetSpan(
+                          alignment:PlaceholderAlignment.middle,//默认是底部
+                            child: Padding(
+                              padding: new EdgeInsets.fromLTRB(0, 0, 20, 0),
+                              child: SizedBox(
+                                width: 10,
+                                height: 50,
+                                child: new Icon(
+                                    itemIcons[index]
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          TextSpan(
+                            text: items[index],
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+
+                      ),
+                    ),
+
+                    Expanded(
+                      child: Text(''), // 中间用Expanded控件
+                    ),
+                    if(index == 0 || index == 2|| index == 4|| index == 5)Icon(Icons.arrow_forward),
+                    Container(
+                      height: 50,
+                      width: 10,
+                    )
+
+                  ],
+                ),
+
+              )
+                  );
             },
-            childCount: 10,
+            childCount: 8,
           ),
         )
       ],
     );
   }
-}
 
- _rengerGridView(int index) {
+   _rengerSliverAppBarColumn(){
 
-  switch(index) {
-    case 0:
-      return new FlatButton(
-          child: Text.rich(
+     return new InkWell(
+         onTap: () {
+           userAvatar == null ? debugPrint('登录') : debugPrint('用户信息');
+         },
 
-            TextSpan(
+         child: new Column(
+//                mainAxisAlignment: MainAxisAlignment.start,
+           children: <Widget>[
+             new Row(
+                 mainAxisAlignment: MainAxisAlignment.end, //横轴
+                 crossAxisAlignment: CrossAxisAlignment.center, //纵轴
+                 children: <Widget>[
+                   new Container(
+                     margin:
+                     const EdgeInsets.fromLTRB(0.0, 50.0, 100.0, 0.0),
+                     child: new InkWell(
+                         child: new Icon(
+                           Icons.settings,
+                           size: 20,
+                         ),
+                         onTap: () {
+                           UiUtils.flutterToastNormal("设置");
+                         }),
+                   ),
+                 ]),
+             new Column(
+                 mainAxisAlignment: MainAxisAlignment.center,
+                 children: <Widget>[
+                   new Container(
+                     margin:
+                     const EdgeInsets.fromLTRB(0.0, 30.0, 0.0, 0.0),
+                     child: new InkWell(
+                         child: userAvatar == null
+                             ? new Image.asset(
+                           "assets/images/normal_user_icon.png",
+                           width: 60.0,
+                           height: 60.0,
+                         )
+                             : new Container(
+                           width: 60.0,
+                           height: 60.0,
+                           decoration: new BoxDecoration(
+                               shape: BoxShape.circle, //图片的形状
+                               color: Colors.transparent,
+                               image: new DecorationImage(
+                                   image:
+                                   new NetworkImage(userAvatar),
+                                   fit: BoxFit.cover),
+                               border: new Border.all(
+                                   color: Colors.white, width: 2.0)),
+                         ),
+                         onTap: () =>
+                             UiUtils.flutterToastNormal("点击头像登录")),
+                   ),
+                   new Container(
+                       margin:
+                       const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
+                       child: new InkWell(
+                           child: new Text(
+                             userName == null ? '点击头像登录' : userName,
+                             style: new TextStyle(
+                                 color: Colors.white, fontSize: 16.0),
+                           ),
+                           onTap: () =>
+                               UiUtils.flutterToastNormal("点击头像登录")))
+                 ]),
+           ],
+         ));
+   }
 
-            children: <InlineSpan>[
 
+   _rengerGridView(int index) {
+     switch (index) {
+       case 0:
+         return new FlatButton(
+             child: Text.rich(
+               TextSpan(
+                 children: <InlineSpan>[
 //              WidgetSpan(
 //                  child: SizedBox(
 //                    width: 120,
@@ -123,77 +288,119 @@ class _PersonalCenterPage extends State<PersonalCenterPage> {
 //                        color: Colors.blue,
 //                        child: Center(child: Text('Hello World!'))),
 //                  )),
-              WidgetSpan(
-                  child:
-                  Padding(
-                  padding: new EdgeInsets.fromLTRB(0, 0, 5, 0),
-                      child:   SizedBox(
-                        width: 15,
-                        height: 15,
-                        child: new Image.asset(
-                          "assets/images/plane.png",
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    ),
-                  ),
+                   WidgetSpan(
+                     child: Padding(
+                       padding: new EdgeInsets.fromLTRB(0, 0, 5, 0),
+                       child: SizedBox(
+                         width: 15,
+                         height: 15,
+                         child: new Image.asset(
+                           "assets/images/plane.png",
+                           fit: BoxFit.cover,
+                         ),
+                       ),
+                     ),
+                   ),
 
-              TextSpan(
-                text: '喜爱',
-                  style: TextStyle(
-                  color: Colors.black,
-              ),
-
-              ),
-
-            ],
+                   TextSpan(
+                     text: '喜爱',
+                     style: TextStyle(
+                       color: Colors.black,
+                       fontSize: 14,
+                     ),
+                   ),
+                 ],
 //              recognizer: apGestureRecognizer..onTap = (){//手势事件
 //
 //              }
-          ),
+               ),
+             ),
+             //去掉水波纹
+             highlightColor: Colors.transparent,
+             splashColor:Colors.transparent,
+             onPressed: () => UiUtils.flutterToastNormal("喜爱"));
 
-          ),
+       case 1:
+         return new FlatButton(
+             child: Text(
+               "收藏",
+               style: TextStyle(
+                 color: Colors.black,
+                 fontSize: 14,
+               ),
+             ),
+             //去掉水波纹
+             highlightColor: Colors.transparent,
+             splashColor:Colors.transparent,
+             onPressed: () {
+               UiUtils.flutterToastNormal("收藏");
+             }
 
-        onPressed:() => UiUtils.flutterToastNormal("喜爱")
-      );
+             );
+       case 2:
+         return new FlatButton(
+             child: Text(
+               "精品",
+               style: TextStyle(
+                 color: Colors.black,
+                 fontSize: 14,
+               ),
+             ),
+             //去掉水波纹
+             highlightColor: Colors.transparent,
+             splashColor:Colors.transparent,
+             onPressed: () {
+               UiUtils.flutterToastNormal("精品");
+             });
+       case 3:
+         return new FlatButton(
+           child: Text(
+             "历史",
+             style: TextStyle(
+               color: Colors.black,
+               fontSize: 14,
+             ),
+           ),
+           //去掉水波纹
+           highlightColor: Colors.transparent,
+           splashColor:Colors.transparent,
+           onPressed: () {
+             UiUtils.flutterToastNormal("历史");
+           },
+         );
+     }
+   }
 
-    case 1:
-      return new FlatButton(
-          child: Text(
-            "收藏",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 12,
-            ),),
+   _sliverFixedExtentItemClick(int index){
 
-          onPressed: () {
-            UiUtils.flutterToastNormal("收藏");
-          }
-      );
-    case 2:
-      return new FlatButton(
-          child: Text(
-            "精品",
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 12,
-            ),),
+    UiUtils.flutterToastNormal(items[index]);
+     switch (index){
+       case 0:
 
-          onPressed: () {
-            UiUtils.flutterToastNormal("精品");
-          }
-      );
-    case 3:
-      return new FlatButton(
-        child: Text(
-          "历史",
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 12,
-          ),),
+         break;
+       case 1:
 
-        onPressed: () {
-          UiUtils.flutterToastNormal("历史");
-        },);
-  }
+         break;
+       case 2:
+
+         break;
+       case 3:
+
+         break;
+       case 4:
+
+         break;
+       case 5:
+
+         break;
+       case 6:
+
+         break;
+       case 7:
+
+         break;
+     }
+   }
 }
+
+
